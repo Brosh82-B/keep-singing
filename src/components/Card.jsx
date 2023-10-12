201d9e755f6355841ee449f872337ea10212ce5b
matanarad <mail2matan@gmail.com>
2023-10-12 11:44:37 +0300

    Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import "./Card.css";
 import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
 
-function Card({ image,text,songName,songLink }) {
+function Card({ image,text,songName,songLink,Title,Poster }) {
   const [show, setShown] = useState(false);
 
   const props3 = useSpring({
@@ -20,10 +20,10 @@ function Card({ image,text,songName,songLink }) {
       onMouseLeave={() => setShown(false)}
     >
       <img src={image} alt="" />
-      <h2 style={{marginTop:"1rem"}}>{text}</h2>
-        <p>טקסט טקסט טקסט טקסט טקסט טקסט טקסט טקסט</p>
+      <h2 style={{marginTop:"1rem"}}>{Title}</h2>
+        <p>{text}</p>
         <button className="song-button" onClick={()=>{window.open(songLink)}}>{songName}</button>
-        <p>מתן ארד</p>
+        <p>{Poster}</p>
 
     </animated.div>
   );
